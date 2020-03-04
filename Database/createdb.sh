008d0a4fcc103ecdde314f96a31ee2bb6ca33cea
Basil Dalié <basil.dalie@etu.umontpellier.fr>
2020-03-04 13:37:56 +0100

    Added database server and api code

---
+++ Database/createdb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mongoimport --db TOL --collection members --file members.json --jsonArray --drop
+mongoimport --db TOL --collection images --file images.json --jsonArray --drop
+mongoimport --db TOL --collection binaries --file binaries.json --jsonArray --drop
+mongoimport --db TOL --collection comments --file comments.json --jsonArray --drop
---
+++ Server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectId;
+const url = "mongodb://localhost:27017";
+
+MongoClient.connect(url, {useNewUrlParser: true , useUnifiedTopology: true }, (err, client) => {
+    let db = client.db("TOL");
+
+    app.get("/members/:mail/:password", (req, res) => {
+	console.log("/members/" + req.params.mail + "/" + req.params.password);
+	
+	try {
+	    db.collection("members").find().toArray((err, documents) => {
+		for (let document of documents) {
+		    if (document.mail === req.params.mail && document.password === req.params.password) {
+			res.end(JSON.stringify(document));
+		    }
+		}
+
+		res.status(404);
+		res.end(JSON.stringify({}));
+	    });
+	} catch (e) {
+	    console.log("Error on /members/:mail/:password : " + e);
+	    res.end(JSON.stringify([]));
+	}
+    });
+});
+
+app.listen(8888);

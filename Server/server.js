bd15bd730a15b2cc59c25f753bea0469dc160988
Squalli karim <rusted1012@gmail.com>
2020-04-04 19:19:59 +0200

    ajout route pour telecharger executable

--- Server/server.js
+++ Server/server.js
@@ -7,10 +7,12 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 });
+app.use(express.static('datas'));
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectId;
 const url = "mongodb://localhost:27017";
+
 const dlOptions = {
     root : __dirname + '/datas',
     dotfiles: 'deny'
@@ -20,48 +22,48 @@ MongoClient.connect(url, {useNewUrlParser: true , useUnifiedTopology: true }, (e
     let db = client.db("TOL");
 
     app.get("/members/:mail/:password", (req, res) => {
-	console.log("/members/" + req.params.mail + "/" + req.params.password);
-	
-	try {
-	    db.collection("members").find().toArray((err, documents) => {
-		console.log(documents);
-		for (let document of documents) {
-		    if (document.mail === req.params.mail && document.password === req.params.password) {
-			res.end(JSON.stringify(document));
-		    }
-		}
+		console.log("/members/" + req.params.mail + "/" + req.params.password);
 
-		res.status(404);
-		res.end(JSON.stringify({}));
-	    });
-	} catch (e) {
-	    console.log("Error on /members/:mail/:password : " + e);
-	    res.end(JSON.stringify([]));
-	}
+		try {
+			db.collection("members").find().toArray((err, documents) => {
+			console.log(documents);
+			for (let document of documents) {
+				if (document.mail === req.params.mail && document.password === req.params.password) {
+				res.end(JSON.stringify(document));
+				}
+			}
+
+			res.status(404);
+			res.end(JSON.stringify({}));
+			});
+		} catch (e) {
+			console.log("Error on /members/:mail/:password : " + e);
+			res.end(JSON.stringify([]));
+		}
     });
 
     app.get("/binaries", (req, res) => {
-	console.log("/binaries");
+		console.log("/binaries");
 
-	try {
-	    db.collection("binaries").find().toArray((err, documents) => {
-		res.end(JSON.stringify(documents));
-	    });
-	} catch (e) {
-	    console.log("Error on /binaries");
-	    res.end(JSON.stringify([]));
-	}
+		try {
+			db.collection("binaries").find().toArray((err, documents) => {
+			res.end(JSON.stringify(documents));
+			});
+		} catch (e) {
+			console.log("Error on /binaries");
+			res.end(JSON.stringify([]));
+		}
     });
 
     app.get("/binaries/:file", (req, res) => {
-	res.sendFile('binaries/' + req.params.file, dlOptions, function(err) {
-	    if (err) {
-		res.status(err.status);
-		res.end();
-	    } else {
-		console.log('Info: Sent binary /binaries/' + req.params.file);
-	    }
-	});
+		res.sendFile('binaries/' + req.params.file, dlOptions, function(err) {
+			if (err) {
+			res.status(err.status);
+			res.end();
+			} else {
+			console.log('Info: Sent binary /binaries/' + req.params.file);
+			}
+		});
     });
 });
 

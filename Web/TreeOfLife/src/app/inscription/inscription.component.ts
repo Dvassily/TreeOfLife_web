92b2c630ddb2531515b08db7cd1c18e9e4a18787
Stefanovski <stefanovski_stefan@hotmail.com>
2020-04-04 22:19:38 +0200

    inscription frontend back end

---
+++ Web/TreeOfLife/src/app/inscription/inscription.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+export class InscriptionComponent implements OnInit {
+
+  constructor(private auth: AuthenticationService,private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  inscriptionrUser(event){
+    event.preventDefault()
+    const errors = []
+    const target = event.target
+    const nom = target.querySelector('#Nom').value
+    const prenom = target.querySelector('#Prenom').value
+    const email = target.querySelector('#Email').value
+    const mdp = target.querySelector('#Motdepasse').value
+    //const cmdp = target.querySelector('#CMotdepasse').value
+/*
+    if(mdp != cmdp){
+      console.error("les mots de passe sont differents");
+      errors.push("Les mots de passe sont differents")      
+    }*/
+    if(errors.length === 0){
+      this.auth.inscriptionUser(nom,prenom,email,mdp).subscribe(data =>{
+        console.log(data)
+        
+       // this.router.navigate(['/app'])
+      
+      })
+    }
+    console.log(nom,prenom,email,mdp)
+  }
+}

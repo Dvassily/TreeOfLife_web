582abd6bae9c6e4a185130f3980abe83bdea85f5
Stefanovski <stefanovski_stefan@hotmail.com>
2020-04-04 22:58:51 +0200

    verification mail

--- Server/server.js
+++ Server/server.js
@@ -64,25 +64,48 @@ MongoClient.connect(url, {useNewUrlParser: true , useUnifiedTopology: true }, (e
 	});
 	});
 	
+	let Existsmail = false;
 	app.get("/inscriptions/:nom/:prenom/:email/:mdp", (req, res)=>{
 		console.log("/inscriptions/" + req.params.email + "/" + req.params.mdp);
 		try{
-			db.collection("members").insertOne({
-	 			mail : req.params.email,
-				name : req.params.nom,
-				firstname : req.params.prenom,
-				password : req.params.mdp
-		});		
-			console.log("client inserer")
-		res.end(JSON.stringify([]));
+			db.collection("members").find().toArray((err, documents) => {
+				//console.log(documents);
+				for (let document of documents) {
+					if (document.mail === req.params.email) {
+					Existsmail = true;
+					console.log("Le mail existe deja");
+					res.status(404);
+					res.end(JSON.stringify({}));
+					}
+				}
+				
 
-		}catch(e){
+				if(!Existsmail){
+					try{
+						db.collection("members").insertOne({
+							 mail : req.params.email,
+							name : req.params.nom,
+							firstname : req.params.prenom,
+							password : req.params.mdp
+					});		
+						console.log("client inserer")
+					res.end(JSON.stringify([]));
+			
+					}catch(e){
+			
+						console.log("Error with inscriptions/");
+						res.end(JSON.stringify([]));
+					}
+				}
+			});
 
-			console.log("Error with inscriptions/");
-			res.end(JSON.stringify([]));
+		
+		} catch (e) {
+	    	console.log("Error on /members/:mail/:password : " + e);
+	    	res.end(JSON.stringify([]));
 		}
 
-	} );
+	});
 });
 
 app.listen(8888);

59d20e8b4123d3985fd4e508ab391e5e4c8d0f41
Squalli karim <rusted1012@gmail.com>
2020-04-06 18:11:56 +0200

    remove unsed route

--- server/server.js
+++ server/server.js
@@ -35,27 +35,6 @@ const url = "mongodb://localhost:27017";
 MongoClient.connect(url, {useNewUrlParser: true , useUnifiedTopology: true }, (err, client) => {
     let db = client.db("TOL");
 
-    app.get("/members/:mail/:password", (req, res) => {
-		console.log("/members/" + req.params.mail + "/" + req.params.password);
-
-		try {
-			db.collection("members").find().toArray((err, documents) => {
-			console.log(documents);
-			for (let document of documents) {
-				if (document.mail === req.params.mail && document.password === req.params.password) {
-				res.end(JSON.stringify(document));
-				}
-			}
-
-			res.status(404);
-			res.end(JSON.stringify({}));
-			});
-		} catch (e) {
-			console.log("Error on /members/:mail/:password : " + e);
-			res.end(JSON.stringify([]));
-		}
-    });
-
     app.get("/binaries", (req, res) => {
 		console.log("/binaries");
 
